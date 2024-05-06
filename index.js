a6cec497b21af5f250987b69d0cb935d7c587a4c
dff5d <lsjcukastestacc@hotmail.com>
2024-05-07 05:23:05 +0700

    test project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("world");
+function sortedListToBST(head) {
+  if (!head) return null;
+  if (!head.next) return new TreeNode(head.val);
+  let slow = head;
+  let fast = head;
+  let prev = null;
+  while (fast && fast.next) {
+    prev = slow;
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  const root = new TreeNode(slow.val);
+  prev.next = null;
+  root.left = sortedListToBST(head);
+  root.right = sortedListToBST(slow.next);
+  return root;
+}
